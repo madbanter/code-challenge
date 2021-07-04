aa7d48c15a9b47f5f948578dce38c66266cba961
Adam Brent <adam.brent@gmail.com>
2021-07-04 16:16:35 -0700

    Adds HTML elements for username and password fields, adds functionality to include them in handleSubmit

--- src/pages/create_account.tsx
+++ src/pages/create_account.tsx
@@ -3,11 +3,16 @@ import { FormEvent } from 'react';
 import styles from 'src/styles/create_account.module.scss';
 
 export default function CreateAccount() {
-  async function handleSubmit(evt: FormEvent) {
+  async function handleSubmit(evt: FormEvent<HTMLFormElement>) {
     evt.preventDefault();
+
+    const username = evt.currentTarget.username.value;
+    const password = evt.currentTarget.password.value;
+
+    console.log(username, password);
     const response = await fetch('/api/create_new_account', {
       method: 'POST',
-      body: JSON.stringify({}),
+      body: JSON.stringify({username, password}),
     });
 
     console.log(await response.json());
@@ -20,6 +25,11 @@ export default function CreateAccount() {
       </Head>
       <article className={styles.article}>
         <form className={styles.form} onSubmit={handleSubmit}>
+          <h1>Create New Account</h1>
+          <label htmlFor="username">Username</label>
+          <input type="text" name="username" placeholder="E.g. new_user1234"/>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" placeholder="E.g. Donotusethispassword1000!"/>
           <button>Create Account</button>
         </form>
       </article>
