7a5ba171018d78456d587287a06704a5ace10e9d
Adam Brent <adam.brent@gmail.com>
2021-07-04 18:20:26 -0700

    Refactor to move the validity checks to their own module, adds additional password checks

--- src/pages/api/create_new_account.ts
+++ src/pages/api/create_new_account.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { Validation, validateCredentials, validations } from './validity_checks';
 
 interface CreateNewAccountParameters {
   username: string;
@@ -8,13 +9,6 @@ interface CreateNewAccountParameters {
 interface BooleanResult {
   result: boolean;
   errors?: Record<string, string>;
-}
-
-interface Validation<T> {
-  check: (criterion: T) => boolean;
-  field: string;
-  errorName: string;
-  error: string;
 }
 
 const exposedPasswordCheck = async function(password: string) {
@@ -37,40 +31,6 @@ export default function createNewAccount(req: NextApiRequest, res: NextApiRespon
   } as const;
   const credentialCheck = validateCredentials(credentials, validations);
   res.status(200).json({ result: credentialCheck.result, errors: credentialCheck.errors });
-}
-
-const validateCredentials = (credentials: CreateNewAccountParameters, criteria: Validation<string>[]): BooleanResult => {
-  const validityChecks = { result: true, errors: {} };
-  for (const [key, value] of Object.entries(credentials)) {
-    const checks = criteria.filter(criterion => criterion.field === key);
-    const credentialResult = validateCredential(value, checks);
-    validityChecks.result = validityChecks.result && credentialResult.result;
-    validityChecks.errors = { ...validityChecks.errors, ...credentialResult.errors };
-  }
-  console.log(validityChecks);
-  return validityChecks;
-}
-
-const validateCredential = (credential: string, criteria: Validation<string>[]): BooleanResult => {
-  const checkedResults = criteria.map(criterion => {
-    const checkResult = criterion.check(credential);
-    return (
-      {
-        result: checkResult,
-        errors: checkResult ? null : { [criterion.errorName]: criterion.error }
-      }
-    )
-  });
-  let result = { result: checkedResults.every(checkedResult => checkedResult.result === true) } as BooleanResult;
-  if (!result.result) {
-    result.errors = {};
-    checkedResults.filter(checkedResult => checkedResult.result === false).forEach(checkedResult => {
-      for (const [errorName, error] of Object.entries(checkedResult.errors)) {
-        result.errors[errorName] = error;
-      }
-    });
-  }
-  return result;
 }
 
 // const validateCredential = async (credential: string, criteria: Validation<string>[]): Promise<BooleanResult> => {
@@ -96,32 +56,3 @@ const validateCredential = (credential: string, criteria: Validation<string>[]):
 //     return result;
 //   });
 // }
-
-const usernameChecks = [
-  {
-    check: (username: string) => 10 <= username.length && username.length <= 50,
-    field: 'username',
-    errorName: 'BadUsernameLength',
-    error: 'Username must be at least 10 and at most 50 characters long.'
-  }
-] as const;
-
-
-// Add checks
-// Fix async for API calls to password_exposed endpoint
-const passwordChecks = [
-  {
-    check: (password: string) => 20 <= password.length && password.length <= 50,
-    field: 'password',
-    errorName: 'BadPasswordLength',
-    error: 'Password must be at least 20 and at most 50 characters long.'
-  },
-  // {
-  //   check: exposedPasswordCheck,
-  //   field: 'password',
-  //   errorName: 'PasswordExposed',
-  //   error: 'Password provided has been exposed! Please select another password.'
-  // },
-] as const;
-
-const validations = [...usernameChecks, ...passwordChecks];
---
+++ src/pages/api/validity_checks.ts
@@ -0,0 +1,134 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+interface CreateNewAccountParameters {
+  username: string;
+  password: string;
+}
+
+interface BooleanResult {
+  result: boolean;
+  errors?: Record<string, string>;
+}
+
+export interface Validation<T> {
+  check: (criterion: T) => boolean;
+  field: string;
+  errorName: string;
+  error: string;
+}
+
+const exposedPasswordCheck = async function(password: string) {
+  const response = await fetch('http://localhost:3000/api/password_exposed', {
+    method: 'POST',
+    body: JSON.stringify({password}),
+  })
+  const resolved = await response.json()
+  // return resolved.result === false;
+  const test = resolved.result === false;
+  console.log(test)
+  return test
+};
+
+export const validateCredentials = (credentials: CreateNewAccountParameters, criteria: Validation<string>[]): BooleanResult => {
+  const validityChecks = { result: true, errors: {} };
+  for (const [key, value] of Object.entries(credentials)) {
+    const checks = criteria.filter(criterion => criterion.field === key);
+    const credentialResult = validateCredential(value, checks);
+    validityChecks.result = validityChecks.result && credentialResult.result;
+    validityChecks.errors = { ...validityChecks.errors, ...credentialResult.errors };
+  }
+  return validityChecks;
+}
+
+export const validateCredential = (credential: string, criteria: Validation<string>[]): BooleanResult => {
+  const checkedResults = criteria.map(criterion => {
+    const checkResult = criterion.check(credential);
+    return (
+      {
+        result: checkResult,
+        errors: checkResult ? null : { [criterion.errorName]: criterion.error }
+      }
+    )
+  });
+  let result = { result: checkedResults.every(checkedResult => checkedResult.result === true) } as BooleanResult;
+  if (!result.result) {
+    result.errors = {};
+    checkedResults.filter(checkedResult => checkedResult.result === false).forEach(checkedResult => {
+      for (const [errorName, error] of Object.entries(checkedResult.errors)) {
+        result.errors[errorName] = error;
+      }
+    });
+  }
+  return result;
+}
+
+// const validateCredential = async (credential: string, criteria: Validation<string>[]): Promise<BooleanResult> => {
+//   const results = criteria.map(async criterion => {
+//     const checkResult = await criterion.check(credential);
+//     return (
+//       {
+//         result: checkResult,
+//         errors: checkResult ? null : { [criterion.errorName]: criterion.error }
+//       }
+//     )
+//   });
+//   await Promise.all(results).then(values => {
+//     let result = { result: values.every(item => item.result === true) } as BooleanResult;
+//     if (!result.result) {
+//       result.errors = {};
+//       values.filter(item => item.result === false).forEach(item => {
+//         for (const [errorName, error] of Object.entries(item.errors)) {
+//           result.errors[errorName] = error;
+//         }
+//       });
+//     }
+//     return result;
+//   });
+// }
+
+export const usernameChecks = [
+  {
+    check: (username: string) => 10 <= username.length && username.length <= 50,
+    field: 'username',
+    errorName: 'BadUsernameLength',
+    error: 'Username must be at least 10 and at most 50 characters long.'
+  }
+] as const;
+
+
+// Add checks
+// Fix async for API calls to password_exposed endpoint
+export const passwordChecks = [
+  {
+    check: (password: string) => 20 <= password.length && password.length <= 50,
+    field: 'password',
+    errorName: 'BadPasswordLength',
+    error: 'Password must be at least 20 and at most 50 characters long.'
+  },
+  {
+    check: (password: string) => /[!@#$%]/.test(password),
+    field: 'password',
+    errorName: 'NeedsSymbol',
+    error: 'Password must contain at least one symbol [!, @, #, $, or %].'
+  },
+  {
+    check: (password: string) => /[a-zA-Z]/.test(password),
+    field: 'password',
+    errorName: 'NeedsLetter',
+    error: 'Password must contain at least one letter [a-z or A-Z].'
+  },
+  {
+    check: (password: string) => /\d/.test(password),
+    field: 'password',
+    errorName: 'NeedsNumber',
+    error: 'Password must contain at least one number [0-9].'
+  },
+  // {
+  //   check: exposedPasswordCheck,
+  //   field: 'password',
+  //   errorName: 'PasswordExposed',
+  //   error: 'Password provided has been exposed! Please select another password.'
+  // },
+] as const;
+
+export const validations = [...usernameChecks, ...passwordChecks];
