93c2b47334c073a46bbdc848086959e1f812ed76
Adam Brent <adam.brent@gmail.com>
2021-07-05 01:22:48 -0700

    Changes messages to dynamically add message type to message body, styling tweaks

--- src/pages/messages.tsx
+++ src/pages/messages.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import styles from 'src/styles/create_account.module.scss';
 
-const processClass = (errorCode): string => {
+const processMessageType = (errorCode): string => {
   let prefix = '';
   if (/^Bad/.test(errorCode) || /^Needs/.test(errorCode)) {
-    prefix = 'red';
+    prefix = 'Error';
   } else if (/^Success/.test(errorCode)) {
-    prefix = 'green';
+    prefix = 'Success';
   } else {
-    prefix = 'yellow';
+    prefix = 'Warning';
   }
-  return styles[`${prefix}Message`];
+  return prefix;
 }
 
 const Message = ({ errorCode, message }): React.ReactElement => {
   // const [text] = Object.values(message);
   // console.log(errorCode, message)
+  const prefix = processMessageType(errorCode);
   return (
-    <li className={processClass(errorCode)}>{message}</li>
+    <div className={styles[`${prefix}Message`]}>
+      <li>{`${prefix}: ${message}`}</li>
+    </div>
   )
 };
 
